1e08762001c3fd30f2ddd58409fc228f6c2318e9
Khenjy DONAT <khenkyr@msn.com>
2022-01-14 16:57:50 +0100

    création de tableau contenant mes articles

---
+++ js/bdd.js
@@ -0,0 +1,122 @@
+import Article from "./article.js";
+
+export const consoles = [
+    new Article("PS5 standard",
+      "../img/console/ps5/ps5_standard",
+      "Console Next Gen pouvant surpuissante avec des graphismes pouvant aller jusqu'à la 8K.",
+      "console ps5",
+      500),
+
+    new Article("PS4 fat",
+      "../img/console/ps4/ps4_fat",
+      "Premier modèle de la PS4, première console afficher de la Full HD.",
+      "console ps4",
+      250),
+
+    new Article("PS3 fat",
+      "../img/console/ps3/ps3_fat",
+      "Premier modèle de PS3, première console HD de sa génération.",
+      "console ps3",
+      100),
+
+    new Article("PS2 fat",
+      "../img/console/ps2/ps2_fat",
+      "Premier modèle de PS2, assez volumineux.",
+      "console ps2",
+      100),
+
+    new Article("PS1",
+      "../img/console/ps1/ps1_console",
+      "Premier modèle de PS1, révolutionnaire de par la possibilité de jouer à des jeux 3D",
+      "console ps1",
+      200),
+
+    new Article("PSP fat",
+      "../img/console/psp/psp_fat",
+      "Premier modèle de PSP, console portable ayant concurrencé la DS.",
+      "console psp",
+      120),
+
+    new Article("PSVITA",
+      "../img/console/psvita/psvita_console",
+      "Sucesseuse de la PSP, n'a pas réussit a s'imposer avec ses nouveautés mais est quand même très cool.",
+      "console psvita",
+      150)
+];
+
+export const accessories = [
+    new Article("Manette DualSense",
+      "../img/accessoires/ps5/manette_ps5",
+      "Manette révolutionnaire qui permet une plus grand immersion avec ses gachettes aptiques et ses vibrations améliorer.",
+      "accessoire ps5",
+      70),
+    new Article("Manette DualShock4",
+      "../img/accessoires/ps4/manette_ps4",
+      "Manette apportant le pad tactile comme nouveauté et des gachettes améliorées.",
+      "accessoire ps4",
+      50),
+    new Article("Manette DualShock3",
+      "../img/accessoires/ps3/manette_ps3",
+      "Manette comportant la technologie Sixaxis qui permet la reconnaissance des mouvement dans l'espace.",
+      "accessoire ps3",
+      40),
+    new Article("Manette DualShock2",
+    "../img/accessoires/ps2/manette_ps2",
+    "Manette qui à apporter les vibrations sur les manettes playstation qui permet une immersion poussé à l'époque.",
+    "accessoire ps2",
+    40),
+    new Article("Manette D-Pad PS1",
+    "../img/accessoires/ps1/manette_ps1",
+    "Manette simpliste avec juste une croix directionnelle et huit boutons.",
+    "accessoire ps1",
+    40),
+    new Article("Memory Stick PSP",
+    "../img/accessoires/psp/memory_stick_psp",
+    "Carte mémoire SD propriétaire de Sony adapté pour la PSP.",
+    "accessoire psp",
+    20)
+];
+
+export const games = [
+  new Article("Demon Souls Remaster",
+    "../img/games/ps5/demon_souls_ps5",
+    "Remaster du célèbre jeu à l'origine sortie sur PS3 des jeu Dark Souls.",
+    "jeu ps5",
+    80),
+
+  new Article("Days Gone",
+    "../img/games/ps4/days_gone_ps4",
+    "Jeu d'action aventure dans un univers apocaplyptique remplis de zombies le tout à bord d'une moto super classe.",
+    "jeu ps4",
+    60),
+
+  new Article("God of War III",
+    "../img/games/ps3/God_of_War_III_ps3",
+    "Incarner Kratos une dernière fois pour anéantir de manière sanglante et brutal les dieux de l'Olympe.",
+    "jeu ps3",
+    10),
+
+  new Article("Jak II",
+    "../img/games/ps2/jak-2-ps2",
+    "Incarner un Jak changé, lourdement armé et se permettant d'emprunter les véhicules des passants sans leur permission.",
+    "jeu ps2",
+    15),
+
+  new Article("Crash Bandicoot",
+    "../img/games/ps1/Crash_Bandicoot_ps1",
+    "Partez sauver votre petite amie capturer par le Dr Neo Cortex et sa grosse tête en tournoyant comme une toupis sans vomir.",
+    "jeu ps1",
+    20),
+
+  new Article("",
+    "../img/games/psp/",
+    "",
+    "jeu psp",
+    10),
+
+  new Article("",
+    "../img/games/psvita/",
+    "",
+    "jeu psvita",
+    30)
+];
---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Article from "./article.js";
+import {consoles, accessories, games} from "./bdd.js";
+/* Partie fonction */
+
+console.log(games);
+
+/* Partie programme */
+document.addEventListener('DOMContentLoaded', () => {
+
+})
