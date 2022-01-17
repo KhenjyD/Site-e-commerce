145bbe115d520444fa4b71bf24185c06f94212c8
Khenjy DONAT <khenkyr@msn.com>
2022-01-17 12:17:43 +0100

    Ajout de l'affichage de tout les items

---
+++ js/item.js
@@ -0,0 +1,57 @@
+export default class Item {
+    constructor(name, img, description, category, price){
+        this.name = name;
+        this.img = img;
+        this.description = description;
+        this.category = category;
+        this.price = price;
+    }
+
+    createCard() {
+        let col = document.createElement("div");
+        col.className = "col mb-5";
+
+        let card = document.createElement("div");
+        card.className = "card";
+        card.style.width = "18rem";
+
+        let img = document.createElement("img");
+        img.src = this.img;
+        img.className = "card-img-top";
+
+        let body = document.createElement("div");
+        body.className = "card-body";
+
+        let title = document.createElement("h5");
+        title.className = "card-title";
+        title.textContent = this.name;
+
+        let ctg = document.createElement("p");
+        ctg.className = "fw-lighter fst-italic";
+        ctg.textContent = "Catégorie: " + this.category;
+
+        let describ = document.createElement("p");
+        describ.className = "card-text";
+        describ.textContent = this.description;
+
+        let price = document.createElement("p");
+        price.className = "fw-bold";
+        price.textContent = "Prix: " + this.price + "euro";
+
+        let buyBtn = document.createElement("button");
+        buyBtn.className = "btn btn-primary rounded-pill";
+        buyBtn.textContent = "Ajouter au panier";
+        buyBtn.id = this.name;
+
+        card.appendChild(img);
+        card.appendChild(body);
+        body.appendChild(title);
+        body.appendChild(ctg);
+        body.appendChild(describ);
+        body.appendChild(price);
+        body.appendChild(buyBtn);
+        col.appendChild(card)
+
+        return col;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,60 @@
-import Article from "./article.js";
-import {consoles, accessories, games} from "./bdd.js";
-/* Partie fonction */
+import {items} from "./bdd.js";
 
-console.log(games);
+/* Partie fonction */
+const displayItemsScreen = () => {
+    document.getElementById("accueil").style.display = "none";
+    document.getElementById("items").style.display = "block";
+}
+
+const displayWelcomeScreen = () => {
+    document.getElementById("accueil").style.display = "block";
+    document.getElementById("items").style.display = "none";
+}
+
+const createCategorySpan = (category) => {
+    let title = document.createElement("h2");
+    let span = document.createElement("span");
+    span.className = "badge bg-info";
+    span.textContent = "Catégorie: " + category;
+    title.appendChild(span);
+
+    return title;
+}
+
+const displayAllItems = () => {
+    displayItemsScreen();
+    let display = document.getElementById("items");
+    display.innerHTML = "";
+    display.appendChild(createCategorySpan("Tout"));
+    let newRow = document.createElement("div");
+    newRow.className = "row";
+
+    items.forEach(item => {
+        newRow.appendChild(item.createCard());
+    });
+    display.appendChild(newRow);
+}
+
+const displayCategoryItems = (category) => {
+    displayItemsScreen();
+    items.forEach(item => {
+        if(item.category == category){
+            item.createCard();
+        }
+    });
+}
 
 /* Partie programme */
 document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById("allItems").addEventListener('click', () => {
+        displayAllItems();
+    })
 
+    document.getElementById("welcomeScreen").addEventListener('click', () => {
+        displayWelcomeScreen();
+    })
+
+    document.getElementById("navbarAll").addEventListener('click', () => {
+        displayAllItems();
+    })
 })
